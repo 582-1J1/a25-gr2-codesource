456532237f3b9612595973cb6c5a6297bed425d3
Semaan <csemaan@cmaisonneuve.qc.ca>
2025-09-04 10:01:11 -0400

    Semaine 3 - Séance 1

---
+++ semaine03/assets/js/main.js
@@ -0,0 +1,32 @@
+// Demander à l'utilisateur quelle est sa province (Québec ou Ontario)
+let sProvince = prompt("Quelle est votre province ? (Q: Québec, ou O: Ontario)");
+
+console.log("Vous avez choisi : ", sProvince);
+
+// Stocker les taux de taxes de vente de chaque province (Québec et Ontario)
+const nTauxTaxeQc = 0.09975;
+const nTauxTaxeOn = 0.08;
+
+
+
+let nMontantAvantTaxes = 100;
+let nMontantTPS = nMontantAvantTaxes * 0.05;
+// let nMontantAvecTPS = nMontantAvantTaxes + nMontantTPS;
+let nMontantTVP = nMontantAvantTaxes * 0.09975;
+let nTotal = nMontantAvantTaxes + nMontantTPS + nMontantTVP;
+
+// Méthode 1 : avec toFixed()
+// let sTotalArrondi = nTotal.toFixed(2);
+// let nTotalArrondi = Number(sTotalArrondi);
+
+// Méthode 2 : avec Math.round()
+let nTotalArrondi = Math.round(nTotal * 100) / 100;
+let nFraisLivraison = 20;
+
+// Affichage
+console.log("Montant original : ", nMontantAvantTaxes);
+console.log("Montant TPS : ", nMontantTPS);
+console.log("Montant TVQ : ", nMontantTVP);
+console.log("Montant total (pas arrondi) : ", nTotal);
+console.log("Montant total arrondi : ", nTotalArrondi);
+console.log("Montant total arrondi plus les frais de livraison : ", nTotalArrondi + nFraisLivraison);
