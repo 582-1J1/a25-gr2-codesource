eb49767d8ac9e99d09ad74f2c3d076fab5dbff05
Semaan <csemaan@cmaisonneuve.qc.ca>
2025-09-18 09:12:32 -0400

    Canvas : ajout d'images.

--- semaine04/jeu-canvas/assets/js/main.js
+++ semaine04/jeu-canvas/assets/js/main.js
@@ -16,11 +16,37 @@ oCtx.fillRect(0, nHauteur/2, nLargeur, nHauteur/2);
 
 // Dessiner le soleil
 oCtx.fillStyle = "yellow";
+// Ombre portée
+oCtx.shadowColor = "yellow";
+oCtx.shadowBlur = 30;
 oCtx.beginPath();
 oCtx.arc(0, 0, 90, 0, Math.PI);
 oCtx.fill();
 
-let sNom = prompt("Quel est votre nom ?");
+
+// Dessiner les rayons :
+// Tracer un chemin en zigzag entre deux cercles et le remplir
+// avec la même couleur que le soleil
+oCtx.fillStyle = "yellow";
+oCtx.beginPath();
+oCtx.moveTo(90, 0);
+oCtx.lineTo(110, 15);
+oCtx.lineTo(85, 30);
+oCtx.lineTo(105, 45);
+oCtx.lineTo(70, 55);
+oCtx.fill();
+
+
+let sNom = "Test"; // prompt("Quel est votre nom ?");
 oCtx.fillStyle = "black";
 oCtx.font = "60px Courrier";
 oCtx.fillText(sNom, nLargeur-200, 50);
+
+// Ajouter une image (créer un objet image et lui associer le fichier 
+// sauvegardé sur le disque)
+let oSoleil = new Image(); // Constructeur (d'objets "images")
+oSoleil.src = "assets/images/soleil-rayons.png";
+
+// Dessiner l'image dans le canvas
+oCtx.drawImage(0, 300);
+
