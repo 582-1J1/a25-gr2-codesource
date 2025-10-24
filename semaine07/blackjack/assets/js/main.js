658e6feb6baed6444c0e3f6a3207148de37f5f0f
Semaan <csemaan@cmaisonneuve.qc.ca>
2025-10-24 09:21:26 -0400

    Générer nombre aléatoires ditincts ou pas avec un paramètre additionnel.

--- semaine07/blackjack/assets/js/main.js
+++ semaine07/blackjack/assets/js/main.js
@@ -12,25 +12,59 @@ const oCtx = oCanvas.getContext("2d");
 // Utiliser la fonction pour générer une combinaison de la loterie 6/49
 
 // Récupérer la valeur retournée par la fonction et l'envoyer à la console
-console.log("Une combinaison de 6/49 : ", genererNombresAleatoires(6, 1, 49));
-console.log("Une combinaison de 6/49 : ", genererNombresAleatoires(6, 1, 49));
-console.log("Une combinaison de 6/49 : ", genererNombresAleatoires(6, 1, 49));
-console.log("25 nombres entre 1000 et 1500 : ", genererNombresAleatoires(25, 1000, 1500));
-console.log("3 nombres entre 10 et 20 : ", genererNombresAleatoires(3, 10, 20));
-console.log("1000 nombres entre 5 et 7 : ", genererNombresAleatoires(1000, 5, 7));
-
-
+console.log("Une combinaison de 6/49 (distincts) : "
+    , genererNombresAleatoires(6, 1, 49, true));
+console.log("Une combinaison de 6/49 (distincts) : "
+    , genererNombresAleatoires(6, 1, 49, true));
+console.log("Une combinaison de 6/49 (PAS DISTINCTS) : "
+    , genererNombresAleatoires(6, 1, 49));
+console.log("Une combinaison impossible (15, 100, 13) : "
+    , genererNombresAleatoires(15, 100, 13, false));
 
 
 // Déclaration de fonction
 // Définir une fonction très générale qui génère des nombres aléatoires
 // IMPORTANT : Nom de la fonction, noms des paramètres, valeur de retour
-function genererNombresAleatoires(nQuantite, nValMin, nValMax) {
-    let sChoix = "";
+function genererNombresAleatoires(nQuantite, nValMin, nValMax, bDistincts = false) {
+    let sChoix = ";";
+    // Validation de base
+    if(nValMax < nValMin) {
+        // On arrête la fonction et retourne un message d'erreur.
+        return "La valeur maximale ne peut pas être plus petite que la valeur minimale.";
+    }
+    // Si nQuantite est plus grand que l'intervalle entre nValMin et nValMax et bDistincts est vrai.
+    let nbAlea = 0;
     for(let i=0; i<nQuantite; i++) {
-        
-        sChoix = sChoix + (Math.floor(Math.random()*(nValMax-nValMin+1)) + nValMin) + ";";
+        nbAlea = Math.floor(Math.random()*(nValMax-nValMin+1)) + nValMin;
+        if(bDistincts == true && sChoix.indexOf(`;${nbAlea};`) == -1) {
+            sChoix = sChoix + nbAlea + ";";
+        }
+        else if(bDistincts == false) {
+            sChoix = sChoix + nbAlea + ";";
+        }
+        else {
+            i--
+        }
     }
     // Valeur retournée par la fonction
     return sChoix;
 }
+
+/*
+function genererNombresAleatoiresDistincts(nQuantite, nValMin, nValMax) {
+    let sChoix = ";";
+    let nbAlea = 0;
+    for(let i=0; i<nQuantite; i++) {
+        nbAlea = Math.floor(Math.random()*(nValMax-nValMin+1)) + nValMin;
+        if(sChoix.indexOf(';' + nbAlea + ';') == -1) {
+            sChoix = sChoix + nbAlea + ";";
+        }
+        else {
+            i--; // on décrémente i pour refaire une itération
+            // i=i-1;
+        }
+    }
+    // Valeur retournée par la fonction
+    return sChoix;
+}
+*/
